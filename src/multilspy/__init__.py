119c9e849ffdec071b1361d6bc9b967b2a4fca88
Jiajun Cheng <125161677+KashunCheng@users.noreply.github.com>
2025-05-27 00:30:02 -0700

    add import for Clangd

--- src/multilspy/__init__.py
+++ src/multilspy/__init__.py
@@ -3,6 +3,7 @@ This module contains the multilspy API
 """
 
 from . import multilspy_types as Types
+from .language_servers.clangd_language_server.clangd_language_server import ClangdLanguageServer
 from .language_server import LanguageServer, SyncLanguageServer
 
-__all__ = ["LanguageServer", "Types", "SyncLanguageServer"]
+__all__ = ["LanguageServer", "Types", "SyncLanguageServer", "ClangdLanguageServer"]
